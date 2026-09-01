da99cd6add996a6757aa8c29c0c71902d1bf0073
harlenst_ <rey999@hotmail.es>
2026-09-01 09:36:25 -0500

    The auctions section is added

---
+++ controllers/auctions.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+
+export const getAuctions = (request: Request, response: Response): void => {
+    console.log(request.query);
+    response.json({
+        ok: true,
+        data: []
+    });
+};
+
+export const getAuction = (request: Request, response: Response): void => {
+    const { id } = request.params;
+    response.json({
+        ok: true,
+        data: {
+            id: id
+        }
+    });
+};
+
+export const createAuction = (request: Request, response: Response): void => {
+    console.log(request.body);
+    response.status(201).json({
+        ok: true,
+        data: request.body
+    });
+};
+
+export const placeBid = (request: Request, response: Response): void => {
+    console.log(request.body);
+    response.status(201).json({
+        ok: true,
+        data: request.body
+    });
+};
+
+export const cancelAuction = (request: Request, response: Response): void => {
+    console.log(request.body);
+    response.status(200).json({
+        ok: true,
+        data: request.body
+    });
+};
