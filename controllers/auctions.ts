af7dcc7a956ceeae56447ef45235e0c264e9c0d8
harlenst_ <rey999@hotmail.es>
2026-09-01 22:22:54 -0500

    update auction controller

--- controllers/auctions.ts
+++ controllers/auctions.ts
@@ -1,34 +1,57 @@
 import { Request, Response } from 'express';
+import { auctions } from '../data/auctions';
 
 export const getAuctions = (request: Request, response: Response): void => {
 
     console.log(request.query);
 
-     response.json({
+    response.json({
         ok: true,
-        data: []
+        data: auctions
     });
 };
-
 export const getAuction = (request: Request, response: Response): void => {
 
     const { id } = request.params;
 
-     response.json({
+    const result = auctions.filter((auction) => {
+        return auction.id.toString() === id;
+    });
+
+    response.json({
         ok: true,
-        data: {
-            id: id
-        }
+        data: result
     });
 };
 
 export const createAuction = (request: Request, response: Response): void => {
 
-    console.log(request.body);
+    const {
+        articleName,
+        articleDescription,
+        articleCondition,
+        categoryId,
+        basePrice,
+        minimumIncrement,
+        closingAt
+    } = request.body;
 
-     response.status(201).json({
+    const newAuction = {
+        id: auctions.length + 1,
+        articleName: articleName,
+        articleDescription: articleDescription,
+        articleCondition: articleCondition,
+        categoryId: categoryId,
+        basePrice: basePrice,
+        minimumIncrement: minimumIncrement,
+        closingAt: closingAt
+    };
+
+    auctions.push(newAuction);
+
+    response.status(201).json({
         ok: true,
-        data: request.body
+        data: newAuction
     });
 };
 
